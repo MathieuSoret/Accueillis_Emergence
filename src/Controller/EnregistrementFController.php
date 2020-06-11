7c8cb5273dddacf2fa8e37c8fecc9e080448a0da
MathieuSoret <msoret37@gmail.com>
2020-06-11 15:40:49 +0200

    Acceuillis

--- src/Controller/EnregistrementFController.php
+++ src/Controller/EnregistrementFController.php
@@ -11,13 +11,11 @@ class EnregistrementFController extends AbstractController
 
     /**
      * @Route("/Enregistrement Famille", name="page.enregistrementF")
-     * @Route("/Enregistrement Acceuilli", name="page.enregistrementA")
      * @return Response
      */
     public function index(): Response
     {
         return $this->render('page/enregistrementF.html.twig');
-        return $this->render('page/enregistrementA.html.twig');
     }
 
 }
