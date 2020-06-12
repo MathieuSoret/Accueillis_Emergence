e78b433d9123e96234dbd4d5e28c04bfa672ef3c
MathieuSoret <msoret37@gmail.com>
2020-06-12 16:23:33 +0200

    Acceuillis

--- src/Controller/EnregistrementSController.php
+++ src/Controller/EnregistrementSController.php
@@ -1,13 +1,16 @@
 <?php
 namespace App\Controller;
 
+use App \Entity \TAccueillis;
 use Symfony \Bundle \FrameworkBundle \Controller \AbstractController;
 use Symfony \ Component \HttpFoundation \Response;
 use Symfony \Component \Routing \Annotation \Route;
 
+
 class EnregistrementSController extends AbstractController
 {
 
+    
 
     /**
      * @Route("/Enregistrement Session", name="page.enregistrementS")
@@ -15,6 +18,14 @@ class EnregistrementSController extends AbstractController
      */
     public function index(): Response
     {
+        $accueillis = new TAccueillis();
+        $accueillis->setQualite('Monsieur')
+        ->setNom('Soret')
+        ->setPrenom('Mathieu')
+        ->setIDAccueilli('1');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($accueillis);
+        $em->flush();
         return $this->render('page/enregistrementS.html.twig');
     }
 
