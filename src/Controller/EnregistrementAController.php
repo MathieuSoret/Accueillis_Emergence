ad1340db71dc39edee7494c78472ec9ac101c542
MathieuSoret <msoret37@gmail.com>
2020-06-12 10:57:54 +0200

    Acceuillis

--- src/Controller/EnregistrementAController.php
+++ src/Controller/EnregistrementAController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller;
 
+use App \Entity \TAccueillis;
+use App \Respository \ TAccueillisRepository;
 use Symfony \Bundle \FrameworkBundle \Controller \AbstractController;
 use Symfony \ Component \HttpFoundation \Response;
 use Symfony \Component \Routing \Annotation \Route;
@@ -8,6 +10,16 @@ use Symfony \Component \Routing \Annotation \Route;
 class EnregistrementAController extends AbstractController
 {
 
+    /**
+     * @var TAccueillisRepository
+     */
+
+    private $repository;
+
+    public function __construct(TAccueillisRepository $repository)
+    {
+        $this->repository = $repository;
+    }
 
     /**
      * @Route("/Enregistrement", name="page.enregistrementA")
@@ -15,6 +27,8 @@ class EnregistrementAController extends AbstractController
      */
     public function index(): Response
     {
+        $property = $this->repository->fond(1);
+        dump($property);
         return $this->render('page/enregistrementA.html.twig');
     }
 
