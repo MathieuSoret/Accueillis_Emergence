4d94823e3c3a138f21891c4b174b69e4b228b383
Kiriuno <mathiskyo37@gmail.com>
2020-06-19 14:17:07 +0200

    Add files via upload

---
+++ src/Controller/NombrePaysController.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Controller;
+
+use App \Entity \TAccueillis;
+use App \Repository \TAccueillisRepository;
+use Doctrine \ORM \EntityManagerInterface;
+use Symfony \Bundle \FrameworkBundle \Controller \AbstractController;
+use Symfony \ Component \HttpFoundation \Response;
+use Symfony \Component \Routing \Annotation \Route;
+
+
+class NombrePaysController extends AbstractController
+{
+
+
+    /**
+     * @var TAccueillisRepository
+     */
+    private $repository;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(TAccueillisRepository $repository, EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/nombrePays", name="page.pageRecherche.nombrePays")
+     * @param TAccueillisRepository $repository
+     * @return Response
+     */
+    public function index(): Response
+    {
+        $property = $this->repository->findAll();
+        return $this->render('page/pageRecherche/nombrePays.html.twig', [
+            'property' => $property
+        ]);
+    }
+
+}
